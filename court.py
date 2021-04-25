9f0555984380c2863a4bbe2c67b27ddaff14c35b
alexuzunov <alex.uzunov2002@gmail.com>
2021-04-25 20:00:55 +0300

    [Update] Added documentation, figures and a few other changes.

--- court.py
+++ court.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('hehe.jpg')
+img = cv2.imread('wow.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 kernel_size = 5
@@ -9,12 +9,12 @@ blur_gray = cv2.GaussianBlur(gray, (kernel_size, kernel_size), 0)
 
 edges = cv2.Canny(blur_gray, 50, 250)
 
-lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 30, np.array([]), maxLineGap=20)
+lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 30, np.array([]), 20)
 print(lines)
 
 for line in lines:
-    x1, y1, x2, y2 = line[0]
-    cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 1)
+     x1, y1, x2, y2 = line[0]
+     cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 1)
 
 
 
